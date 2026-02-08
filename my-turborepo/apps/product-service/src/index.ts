d80117be3b9a07db865918e8e7b31ab536984bb3
jaison <jaisonjohny26@gmail.com>
2026-02-08 15:02:26 +0000

    services added

--- my-turborepo/apps/product-service/src/index.ts
+++ my-turborepo/apps/product-service/src/index.ts
@@ -1,8 +1,24 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+  credentials: true // Allow cookies to be sent with requests
+}))
 
-app.listen(3000, () => {
-  console.log('Product service is running on port 3000');
+app.get('/products', (req, res) => {
+  res.json([
+    { id: 1, name: 'Product 1', price: 10 },
+    { id: 2, name: 'Product 2', price: 20 },
+    { id: 3, name: 'Product 3', price: 30 }
+  ]);
+});
+
+
+app.listen(8000, () => {
+  console.log('Product service is running on port 8000');
 }); 
